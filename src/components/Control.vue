75e3e4579feb5bbf8d7576c737a081b77a946ba2
Eclipssis <eclipssis.star@gmail.com>
2019-12-24 01:43:22 +0200

    init commit

---
+++ src/components/Control.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="constrol">
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Control"
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Control />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Control from "@/components/Control.vue";
+
+export default {
+  name: "home",
+  components: {
+    Control
+  }
+};
+</script>
